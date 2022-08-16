3b7bc9facd24cf779a59777e1bb10f133bda9f34
mejey1 <mejey1@gmail.com>
2022-08-16 21:46:53 +0300

    feat:complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(startN, endN, n) {
+      this.startN = 0;
+      this.endN = 0;
+      this.n = 0;
+    }
+  
     setRange(min, max) {
-
+      this.startN = min;
+      this.endN = max;
     }
-
+  
     guess() {
-
+      if (this.endN - this.startN > 0) {
+        this.n = this.endN - Math.trunc((this.endN - this.startN) / 2);
+      } else {
+        this.n = this.endN;
+      }
+      return this.n;
     }
-
+  
     lower() {
-
+      this.endN = this.n;
     }
-
+  
     greater() {
-
+      this.startN = this.n;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
